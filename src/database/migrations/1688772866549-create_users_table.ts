e04570bc3957d6fdbe59e2f58e9a1b91b166548b
Gabriel Almeida <gabrielgqa@gmail.com>
2023-07-08 00:10:57 +0000

    feat: migrations to permissions control

--- src/database/migrations/1688772866549-create_users_table.ts
+++ src/database/migrations/1688772866549-create_users_table.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
-export class CreateUsers1627508510952 implements MigrationInterface {
+export class CreateUsersTable1688772866549 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
