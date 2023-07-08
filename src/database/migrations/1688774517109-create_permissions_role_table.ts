34cb0062d62b3a96fe76a66f8a71500fbc8220a3
Gabriel Almeida <gabrielgqa@gmail.com>
2023-07-08 00:04:01 +0000

    feat: migrations to permissions control

---
+++ src/database/migrations/1688774517109-create_permissions_role_table.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class CreatePermissionsRoleTable1688774517109 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
---
+++ src/database/migrations/1688774530841-create_users_role_table.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class CreateUsersRoleTable1688774530841 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
